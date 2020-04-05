09de00ae02a8ab78973386f5a659197e07105ac9
bay <bay00@outlook.de>
2020-04-05 17:03:11 +0200

    Updates

---
+++ precache-manifest.e68057a216d42272b934fcb4acc03742.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03c568ac36f66beaacbea2d6e4b9a58",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3d7b05a75d562213c56c",
+    "url": "./static/css/2.527ec867.chunk.css"
+  },
+  {
+    "revision": "3d7b05a75d562213c56c",
+    "url": "./static/js/2.b44666a6.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "./static/js/2.b44666a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de441f655589d457a921",
+    "url": "./static/js/main.bfd14cbb.chunk.js"
+  },
+  {
+    "revision": "1491032a554b53d04d5c",
+    "url": "./static/js/runtime-main.bd1cd1a2.js"
+  },
+  {
+    "revision": "974111fb3b1d2d0533da460c396e6a55",
+    "url": "./static/media/logo192.974111fb.png"
+  }
+]);
